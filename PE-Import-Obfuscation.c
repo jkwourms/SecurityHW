6b8fead683c282d6f814c8fcaffc190e3034812d
Jennifer Wourms <jwourms@infoverity.com>
2019-10-12 20:10:37 -0400

    Using Obfuscation technique to fool malware detection program

---
+++ PE-Import-Obfuscation.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define DATA_SIZE 1000
+
+int main() {
+	FILE * fPtr;
+	char contents[DATA_SIZE];
+	char data[DATA_SIZE];
+	const char *filename = "PE-1.txt";
+	const char *str = "I want to learn PE file format!";
+	int i = 1;
+	
+	while (1) {																			
+		//check if PE-1.txt exists in root directory
+		L1: if (i == 1) {
+			i = (i + 3) / 2;
+			goto L2;
+		}
+		L2: if (access(filename, F_OK) != -1) {
+			// is yes, check contents of file & print to stdout if valid, if not append string to file
+			fPtr = fopen(filename, "r");
+			if(fgets(contents, DATA_SIZE, fPtr) != NULL) {
+				if((strstr(contents, str)) != NULL) {
+					printf("%s\n", contents);
+					return(0);
+				}
+				else {
+					fputs(str, fPtr);
+				}
+			}
+			fclose(fPtr);
+		}
+		// if not, create file
+		else {
+			goto L3;
+		L3: fPtr = fopen(filename, "w");
+			goto L4;
+			
+		L4:	fputs(str, fPtr);
+			fclose(fPtr);
+		}
+	}
+
+	return 0;
+}
